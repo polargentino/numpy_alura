6fefe4d7060588399c3a397755ad9666e77f9a73
Pablo Matías Monsalvo <polargentino@gmail.com>
2025-05-14 09:17:46 -0300

    superstore_2b.py con .csv arreglado

--- superstore_2a.py
+++ superstore_2a.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 datos_seleccion = pd.read_csv('superstore_data.csv', usecols = [0,1,4])
 print(datos_seleccion)
-datos_seleccion.to_csv('clientes_mercado.csv')
+datos_seleccion.to_csv('clientes_mercado.csv', index=False)
 
 '''
          Id  Year_Birth   Income
