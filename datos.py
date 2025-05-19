f33a47891e16bf4b8cde28f4c1e2acba7cce167f
Pablo Matías Monsalvo <polargentino@gmail.com>
2025-05-19 10:11:20 -0300

    datos.py

--- datos.py
+++ datos.py
@@ -13,7 +13,7 @@ print(datos.head())
 '''
 datos = pd.json_normalize(datos['info_inmuebles'])
 print(datos.head())
-
+print(type(datos))
 '''
  evaluacion_general  ...                                             precio
 0               10.0  ...  [$110.00, $45.00, $55.00, $52.00, $85.00, $50....
@@ -21,4 +21,7 @@ print(datos.head())
 2               10.0  ...                                          [$975.00]
 3               10.0  ...  [$490.00, $550.00, $350.00, $350.00, $350.00, ...
 4               10.0  ...                                 [$200.00, $545.00]
+[5 rows x 13 columns]
+
+<class 'pandas.core.frame.DataFrame'>
 '''
